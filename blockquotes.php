bfcf9862fee57802430f82e75fecfa0b66698b18
Hunternabin <hunternabin66@gmail.com>
2018-03-10 18:10:35 +0545

    this is html simple code

---
+++ blockquotes.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>block quotes</title>
+</head>
+<body>
+<blockquote>
+	<p><strong>"The greatest happiness of life is the conviction that we are loved; loved for ourselves, or rather, loved in spite of ourselves."</strong></p>
+</blockquote>
+</body>
+</html>
---
+++ em and strong.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>emphasis and strong example </title>
+	<meta charset="utf-8"/> <!-- or <meta name = "value"></meta> -->
+</head>
+
+<body>
+<p>hello <strong>beautiful </strong>girl and <em>boys good morning </em></p> <!-- em makes word italic and strong make word bold by default of web css -->
+</body>
+</html>
