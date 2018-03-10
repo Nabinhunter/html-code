58976ca8661d3c6aba69244bc937279e9aa59d1a
Nabin Kumar Timalsina <37205570+Nabinhunter@users.noreply.github.com>
2018-03-10 20:37:54 +0545

    simple web site using html and css

---
+++ exercise of simple website.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+ <style>
+	body{
+		background-color: #e3e3e3; /* grey color code is #3e3e3e */
+		/*width: 600px;
+		margin: auto;*/
+	}
+	.nabin {
+		width: 600px; /* anyone can be used like body mai width and margin set garda ni hunxa otherwise .nabin class banayera garda ni hunxa */
+		margin: auto ;
+	}
+blockquote{
+	border: 1px solid white;  /* border: 1px solid white le border dinxa of 1 px;*/
+	text-align: center; /* border inside bhako text ko alignment center ho */
+	margin-left: 0px; 
+}
+
+</style> 
+
+<title>simple website practise </title>
+</head>
+
+<body>
+<div class="nabin"><!-- class rakhda mathi style ma .nabin rakhne nabha id use garema #idname rakhes --> 
+<h1>My First Website </h1>
+<p>welcome to my website! Here i something i enjoy!</p>
+<ul>
+	<li>web development</li>
+	<li>chess</li>
+	<li>learning</li>
+</ul>
+<h3>Fauviorate Quotes</h3>
+<blockquote>
+	<p>
+	Here is a favourite quote of mine!
+
+	</p>
+</blockquote>
+<blockquote>
+	<p>
+	...and another!
+
+	</p>
+</blockquote>
+
+</div>
+</body>
+</html>
